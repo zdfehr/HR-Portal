486d385453e3b2f49ae9388dbc50e72ba6f34dad
Zachary Fehr <zdfehr@gmail.com>
2020-05-21 13:15:41 -0500

    Initial

---
+++ HR PortalTests/HR_PortalTests.swift
@@ -0,0 +1,34 @@
+//
+//  HR_PortalTests.swift
+//  HR PortalTests
+//
+//  Created by Zachary Fehr on 5/21/20.
+//  Copyright © 2020 ZFEHR development. All rights reserved.
+//
+
+import XCTest
+@testable import HR_Portal
+
+class HR_PortalTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
