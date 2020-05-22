f10170916d6b0eff774e8079e6892ec2253143ec
Zachary Fehr <zdfehr@gmail.com>
2020-05-21 20:23:30 -0500

    Storyboard updates

---
+++ HR Portal/ViewController.sync-conflict-20200521-131930-XTXS5ZG.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HR Portal
+//
+//  Created by Zachary Fehr on 5/21/20.
+//  Copyright © 2020 ZFEHR development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
